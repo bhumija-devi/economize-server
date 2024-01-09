b3518c8fb3c8399ee8ca9ba002f5c8b9fc1c3093
bhumija-devi <malleladevi15@gmail.com>
2024-01-09 18:40:02 +0530

    small changes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Use middleware to parse JSON
 app.use(bodyParser.json());
+app.use(cors());
 
 // MySQL connection
 const pool = mysql.createPool({
